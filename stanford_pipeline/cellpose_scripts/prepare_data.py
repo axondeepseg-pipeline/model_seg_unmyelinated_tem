c0397df07756c348c338fa7f6615ca7eddb7a6ab
Armand Collin <armac@videotron.ca>
2026-04-07 11:31:44 -0400

    Update preprocessing script + add skimage dependency

--- stanford_pipeline/cellpose_scripts/prepare_data.py
+++ stanford_pipeline/cellpose_scripts/prepare_data.py
@@ -14,33 +14,6 @@ import numpy as np
 
 CELLPOSE_MASK_SUFFIX = '_seg-cellpose.png'
 
-
-def find_all_images_and_masks(dir_path: Path, mask_suffix: str = '_seg-axonmyelin-manual.png') -> list[tuple[Path, Path]]:
-    """
-    Find all image and corresponding mask file paths in the given directory.
-    
-    Parameters:
-    - dir_path: Path to the directory to search.
-    - mask_suffix: Suffix used to identify mask files.
-    
-    Returns:
-    - List of tuples containing (image_path, mask_path).
-    """
-    image_mask_pairs = []
-    mask_files = list(dir_path.glob(f'*{mask_suffix}'))
-    for mask_file in mask_files:
-        image_file = mask_file.with_name(mask_file.name.replace(mask_suffix, '.png'))
-        if image_file.exists():
-            image_mask_pairs.append((image_file, mask_file))
-        else: 
-            # in case images are in TIFF format instead of PNG
-            image_file_tiff = mask_file.with_name(mask_file.name.replace(mask_suffix, '.tif'))
-            if image_file_tiff.exists():
-                image_mask_pairs.append((image_file_tiff, mask_file))
-            else:
-                print(f'Warning: No corresponding image found for mask {mask_file}')
-    return image_mask_pairs
-
 def convert_axonmyelin_mask_to_cellpose(mask_path: Path, output_path: Path):
     """
     Convert an axon-myelin segmentation mask to a Cellpose-compatible mask.
@@ -62,6 +35,7 @@ def convert_axonmyelin_mask_to_cellpose(mask_path: Path, output_path: Path):
     - output_path: Path to save the converted Cellpose mask.
     """
     mask = Image.open(str(mask_path)).convert("L")
+    mask = np.array(mask)
     cellpose_mask = (mask > 0)  # Set axon and myelin to 1, background to 0
     cellpose_mask = measure.label(cellpose_mask, connectivity=1)  # Label connected components
 
@@ -71,30 +45,47 @@ def convert_axonmyelin_mask_to_cellpose(mask_path: Path, output_path: Path):
     cellpose_mask_img = Image.fromarray(cellpose_mask, mode='I;16')
     cellpose_mask_img.save(str(output_path))
 
+def find_image_mask_pairs_from_bids(dir_path: Path, mask_suffix: str = '_seg-axonmyelin-manual.png') -> list[tuple[Path, Path]]:
+    """
+    Find all image and corresponding mask file paths in a BIDS-like directory structure.
+    
+    Parameters:
+    - dir_path: Path to the root directory to search (should contain subdirectories like 'sub-*/micr/').
+    - mask_suffix: Suffix used to identify mask files.
+    
+    Returns:
+    - List of tuples containing (image_path, mask_path).
+    """
+    image_mask_pairs = []
+    mask_files = list(dir_path.glob(f'derivatives/labels/sub-*/micr/*{mask_suffix}'))
+    for mask_file in mask_files:
+        image_filename = mask_file.name.replace(mask_suffix, '.png')
+        image_file = list(dir_path.glob(f'sub-*/micr/{image_filename}'))[0]
+        if image_file.exists():
+            image_mask_pairs.append((image_file, mask_file))
+        else: 
+            # in case images are in TIFF format instead of PNG
+            image_file_tiff = mask_file.with_name(mask_file.name.replace(mask_suffix, '.tif'))
+            if image_file_tiff.exists():
+                image_mask_pairs.append((image_file_tiff, mask_file))
+            else:
+                print(f'Warning: No corresponding image found for mask {mask_file}')
+    return image_mask_pairs
+
 def preprocess_dataset(data_dir: Path, output_dir: Path, target_class: str = 'myelinated'):
-    train_dir = data_dir / 'train'
-    test_dir = data_dir / 'test'
-    
-    if not train_dir.exists() or not test_dir.exists():
-        raise ValueError("The provided data_dir must contain 'train/' and 'test/' subdirectories.")
-    
-    
-    output_train_dir = output_dir / 'train'
-    output_test_dir = output_dir / 'test'
-    for out_dir in [output_train_dir, output_test_dir]:
-        out_dir.mkdir(parents=True, exist_ok=True)
     
     suffix = '_seg-axonmyelin-manual.png' if target_class == 'myelinated' else '_seg-uaxon-manual.png'
-    train_data = find_all_images_and_masks(train_dir, suffix)
-    test_data = find_all_images_and_masks(test_dir, suffix)
-    
-    for data, output_dir in zip([train_data, test_data], [output_train_dir, output_test_dir]):
-        for image_path, mask_path in data:
-            output_image_path = output_dir / image_path.name
-            output_mask_path = output_dir / (mask_path.stem.replace('_seg-axonmyelin-manual', CELLPOSE_MASK_SUFFIX))
+    train_data = find_image_mask_pairs_from_bids(data_dir, suffix)
 
-            shutil.copy(image_path, output_image_path)
-            convert_axonmyelin_mask_to_cellpose(mask_path, output_mask_path)
+    output_dir = output_dir / target_class
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    for image_path, mask_path in train_data:
+        output_image_path = output_dir / image_path.name
+        output_mask_path = output_dir / (mask_path.name.replace(suffix, CELLPOSE_MASK_SUFFIX))
+
+        shutil.copy(image_path, output_image_path)
+        convert_axonmyelin_mask_to_cellpose(mask_path, output_mask_path)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description="Preprocess dataset for Cellpose")
